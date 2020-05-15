9c31ccc2dfcf729604a27e17d46aec2f5a7d7a3a
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-15 05:26:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names , message){
+  let cards=[];
+  for(let i=0; i<names.length; i++)
+  {
+cards.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+
+  }
+  return cards;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+// second challenge
+
+function countDown(num)
+{
+  while(num>=0)
+  {
+    console.log(num);
+    num--;
+  }
+}

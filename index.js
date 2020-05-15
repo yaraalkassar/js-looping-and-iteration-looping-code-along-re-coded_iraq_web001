ade9c0465474ed7e49c18fc1dc93fe6cc5a82eec
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-15 04:50:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 function writeCards(arr,message) {
   for(let i=0;i<arr.length;i++)
   {
-  return  console.log("Thank you, ${arr[i]}, for the wonderful $message  gift!");
+  console.log("Thank you, ${arr[i]}, for the wonderful $message  gift!");
   }
 }
-
-
-
-
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
